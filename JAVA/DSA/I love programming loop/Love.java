aaa79a9bea02aae65b2eda4e3551a19e2b5e5614
Charlie Achapero <60085492+Charlsshhh@users.noreply.github.com>
2023-10-07 18:59:07 +0000

    dsa

---
+++ JAVA/DSA/I love programming loop/Love.java
@@ -0,0 +1,12 @@
+public class Love {
+    public static void main(String[] args) {
+
+        love();
+    }
+
+    public void love() {
+        for(a = 0; a > 4; a++) {
+            System.out.println("I love Programming");
+        }
+    }
+}
